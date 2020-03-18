eb0cdf60f8e39340ab7978850e28c536d970181b
nanogrunge <gdg007@gmail.com>
2020-03-17 21:19:43 -0300

    Updates

---
+++ precache-manifest.4288ebb30458c2eea7dd6d71c222cfd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7822a10eb2bff000a981be3601a7c3b1",
+    "url": "/crud/index.html"
+  },
+  {
+    "revision": "b6344a3893582a5ff6a8",
+    "url": "/crud/static/js/2.75c906d1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/crud/static/js/2.75c906d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90e4416ccdef84912aaf",
+    "url": "/crud/static/js/main.743bcadb.chunk.js"
+  },
+  {
+    "revision": "0221b9ba87b5d69eb572",
+    "url": "/crud/static/js/runtime-main.28587eb0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/crud/precache-manifest.4cca32f81384d616035b2a46217ad809.js"
+  "/crud/precache-manifest.4288ebb30458c2eea7dd6d71c222cfd4.js"
 );
 
 self.addEventListener('message', (event) => {

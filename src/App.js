fa26241169da3214c87e89d5855eb349b4dfb983
nanogrunge <gdg007@gmail.com>
2020-03-17 21:17:12 -0300

    ''

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import BooksManage from './BooksManage';
+
+
+class App extends React.Component {
+  //state component
+  state = {
+    books : [
+      {id:1, title:'Caballo de Troya', author:'J.J. Benitez', publisher:'Editorial Oceano'},
+      {id:2, title:'Pet Sematary', author:'Stepehen King', publisher:'Editorial Oceano'},
+      {id:3, title:'Dr Jekyll and Mr Hyde', author:'Robert Louis Stevenson', publisher:'Editorial Oceano'}
+    ]
+  }
+//function to add a new book
+addBook = (book) => {
+    let books = [...this.state.books, book];
+    let indice = books.length;
+    book.id = indice;
+    this.setState({
+           books
+        })
+  } 
+//function to delete books
+deleteBooks =(id) => {
+  const books = this.state.books.filter(book =>{
+    return book.id !== id   
+  });
+   //let indx =books.length;
+   let i=1;
+   books.map(book => book.id = i++);
+  this.setState({
+    books
+  })
 }
+//updateBooks
+updateBooks = (id) => {
+  const books = this.state.books.filter(book => {
+    
+    return book.id == id
+  });
+  
+}     
+//function to re-update books
+updateList = (book) => {
+  let bookaroo = book; 
+  let books = [...this.state.books];
+  let id = bookaroo.id;
+  books.map (book =>{
+    if(book.id == id){
+      book.id = bookaroo.id;
+      book.title = bookaroo.title;
+      book.author = bookaroo.author;
+      book.publisher = bookaroo.publisher;   
+      this.setState({
+        books
+      })
+    }
+  });
+  }
 
+  render(){
+  return (
+      <div className="App Container">
+        <h1 className="center blue-text">Bookstore</h1>
+       
+        <BooksManage books={this.state.books} addBook = {this.addBook} deleteBooks = {this.deleteBooks} updateBooks={this.updateBooks }  updateList ={this.updateList}/>
+        
+      </div>
+    );
+  }
+}
 export default App;
---
+++ src/BooksManage.js
@@ -0,0 +1,120 @@
+import React from 'react' ;
+
+class BooksManage extends  React.Component {
+//state component
+state = {
+    id:'',title:'', author:'', publisher:''
+}
+
+//function to handle submit
+handleSubmit = (e) => {
+    e.preventDefault();
+    let title = document.getElementById('title').value;
+    let aut = document.getElementById('author').value;
+    let pub = document.getElementById('publisher').value;
+    if(title === '' || aut === '' || pub === '') {
+        alert('Please fill in all fields', 'danger');
+      } else {
+    this.props.addBook(this.state);  
+    this.setState({
+       id:'', title:'', author:'', publisher:''
+    })
+ } 
+}
+//function for input
+handleTitle = (e) => {
+this.setState({
+    title:e.target.value
+    })
+}
+//
+handleAt = (e) => {
+    this.setState({
+        author:e.target.value
+    })
+}
+handlePublisher = (e) => {
+    this.setState({
+        publisher:e.target.value
+        })
+    }
+//function to handle updates
+handleUpdates = () =>{
+    console.log('im working');
+    this.props.updateList(this.state);
+    this.setState({
+        id:'', title:'', author:'', publisher:''
+     })
+     document.getElementById('updbtn').style.display= 'none';
+     document.getElementById('subbtn').style.display = 'block';
+}
+//------------test function-----------
+
+//end of functions
+render(){
+    
+    const {books, deleteBooks,updateBooks} = this.props;
+
+
+    const handleBooks = (book) => {
+        document.getElementById('subbtn').style.display= 'none';
+        document.getElementById('updbtn').style.display = 'block';
+        this.setState({
+          id:book.id ,  title:book.title, author:book.author, publisher:book.publisher
+            
+        })     
+      }  
+
+            const booksList = books.length ? (
+            
+            books.map(book =>{
+                return(
+                            <tr className='item collection'    key ={book.id}>             
+                                <td className='col s1'>{book.id}</td>
+                                <td  className='col s6'>{book.title}</td>
+                                <td  className='col s4'> {book.author}</td>
+                                <td  className='col s1 left'> {book.publisher}</td>
+                                <td><i onClick ={()=>{deleteBooks(book.id)}} className="right material-icons small red-text">close</i></td>
+                                <td> <button onClick={()=>{updateBooks(book.id); handleBooks(book,book.id)}}  className='btn-small secondary-content green'> edit </button></td>
+                            </tr>      
+                )
+            })
+        ):(
+            <p className='center green-text'> there are no books </p>
+        )
+         
+    return (
+        <div className='container' >
+          <table  >
+             <thead className='blue-text center' > 
+                 <tr>
+                     <th>id</th>
+                     <th>Title</th>
+                     <th>Author/Writer</th>
+                     <th>Publisher</th>
+                     <th></th>
+                     <th></th>
+                 </tr>
+             </thead>
+             <tbody>
+                 {booksList}
+             </tbody>
+         </table>                
+         <div>
+                <form id="formu" >
+                    <label> add new book</label>
+                    <input id='title' type="text" placeholder='Title' onChange={this.handleTitle}  value ={this.state.title}/>
+                    <input id='author'type="text" placeholder='author' onChange={this.handleAt} value ={this.state.author}/>
+                    <input id='publisher'type="text" placeholder='Publisher' onChange={this.handlePublisher} value ={this.state.publisher}/>
+                <button id='subbtn' onClick={this.handleSubmit} className='btn small blue'> Submit </button> 
+                       
+                </form>
+            <button id='updbtn' style={{display:'none'}}  onClick={this.handleUpdates} className="btn-small green"> Update</button>
+            </div>
+ 
+        </div>
+        )
+    } 
+}
+
+export default BooksManage;
